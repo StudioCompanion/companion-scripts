9f8033ef6172798c21e6953ed616f552ff3faa50
Josh <37798644+joshuaellis@users.noreply.github.com>
2021-05-26 12:12:26 +0100

    fix: only export typescript for CJS

--- packages/eslint-config/src/index.js
+++ packages/eslint-config/src/index.js
@@ -1,5 +1,22 @@
-import * as typescript from './typescript-react'
-import * as javascript from './javascript-react'
-
-export default typescript
-export { javascript }
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'react', 'prettier', 'react-hooks'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      ecmaVersion: 2018,
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+  rules: {},
+}
