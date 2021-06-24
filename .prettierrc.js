e1572a9a14064efd5a4819f8edef6a724bcdaf37
Josh <37798644+joshuaellis@users.noreply.github.com>
2021-06-24 16:16:55 +0100

    feat: eslint didnt have react or typescript recommended

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  ...require('./packages/prettier/src/index.js'),
-  overrides: [
-    {
-      files: '.prettierrc',
-      options: { parser: 'json' },
-    },
-  ],
-}
--- packages/eslint-config/src/index.js
+++ packages/eslint-config/src/index.js
@@ -7,6 +7,8 @@ module.exports = {
   extends: [
     'prettier',
     'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
   ],
   plugins: ['@typescript-eslint', 'react', 'prettier', 'react-hooks'],
@@ -18,5 +20,26 @@ module.exports = {
     },
     sourceType: 'module',
   },
-  rules: {},
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'require-await': 'off',
+    '@typescript-eslint/require-await': 'warn',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    'no-unused-vars': 'off',
+    'prefer-const': 'error',
+    'no-duplicate-imports': 'error',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+    'react/prop-types': 'off',
+  },
 }
